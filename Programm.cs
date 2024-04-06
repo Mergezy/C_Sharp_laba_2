5dbe208aee79eb38d5f3c4b3dea79cfbfd8ade07
M3rg <123715188+Mergezy@users.noreply.github.com>
2024-04-07 00:32:23 +0300

    Create Programm.cs

---
+++ Programm.cs
@@ -0,0 +1,37 @@
+static void Main(string[] args)
+{
+    byte day = 8;
+    while(day > 7 ) 
+    {
+        Console.Write("Введите день недели от 1 до 7: ");
+        day = Convert.ToByte(Console.ReadLine());
+    }
+    Console.Write("Введите время в 24 формате: ");
+    byte dayTime = Convert.ToByte(Console.ReadLine());
+
+    Console.WriteLine(getInformation((Day)(day -= 1), dayTime));
+}
+private enum Day
+{
+    понедельник = 0, 
+    вторник, 
+    среда, 
+    четверг, 
+    пятница, 
+    суббота,
+    воскресенье
+}
+private enum DayTime
+{
+    утро = 1, 
+    обед, 
+    вечер,
+    ночь
+}
+static string getInformation(Day day, byte dayTime) 
+{
+    if (dayTime >= 7 && dayTime <= 12) { return $"Сейчас {day}, {DayTime.утро}"; }
+    if (dayTime >= 13 && dayTime <= 18) { return $"Сейчас {day}, {DayTime.обед}"; }
+    if (dayTime >= 19 && dayTime <= 23) { return $"Сейчас {day}, {DayTime.вечер}"; }
+    return $"Сейчас {day}, {DayTime.ночь}";
+}

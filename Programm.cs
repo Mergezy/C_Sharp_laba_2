4cb66fe179a2b001cc1eee36110f3f84f6b5f4b2
M3rg <123715188+Mergezy@users.noreply.github.com>
2024-04-08 12:12:59 +0300

    Update Programm.cs

--- Programm.cs
+++ Programm.cs
@@ -33,6 +33,6 @@ static string getInformation(Day day, byte dayTime)
     if (dayTime >= 7 && dayTime <= 12) { return $"Сейчас {day}, {DayTime.утро}"; }
     if (dayTime >= 13 && dayTime <= 18) { return $"Сейчас {day}, {DayTime.обед}"; }
     if (dayTime >= 19 && dayTime <= 23) { return $"Сейчас {day}, {DayTime.вечер}"; }
-    if (dayTime >= 25) { return "Неверный формат времени"; }
+    if (dayTime >= 24) { return "Неверный формат времени"; }
     return $"Сейчас {day}, {DayTime.ночь}";
 }
